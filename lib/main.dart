8e2acf36e3f283a73ec15c42daa4e92b7ab8b71c
dao0203 <youzaizuoteng872@gmail.com>
2024-02-16 02:47:29 +0900

    configure supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:system_design_app/feature/home/home_router.dart';
 import 'package:system_design_app/theme/color_schemes.g.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_KEY']!,
+  );
   runApp(MyApp());
 }
 
